d827364f0230e7ab78af08202443f547dd52fed0
luvingg <Luvriott@gmail.com>
2025-04-14 13:02:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,6 +11,6 @@ module.exports = {
     ],
     songStatus: true
   },
-  spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
-  spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
+  spotifyClientId: "d0dfc0122b3a4da28fdb878e65d69767",
+  spotifyClientSecret: "7ec4041a8d7640b2991d631116d3876f",
 }
